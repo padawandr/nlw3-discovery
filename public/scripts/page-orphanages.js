564e3a95850132c25a7238202c538c60e0a31423
Wanderley Rabelo <padawandr@gmail.com>
2020-10-14 15:44:39 -0300

    Add map to orphanages page

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,23 @@
+const map = L.map("mapid").setView([-15.7932834, -47.8865424], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create popup
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minHeight: 240,
+  minWidth: 240,
+}).setContent(
+  'Nosso Lar <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg" alt=""></a>'
+);
+
+L.marker([-15.7932834, -47.8865424], { icon }).addTo(map).bindPopup(popup);
